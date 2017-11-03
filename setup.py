33d4679a766c7c5154909056eb9ac0096aa5dddf
iory <ab.ioryz@gmail.com>
2017-11-03 11:21:37 +0900

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+
+import os
+import pkg_resources
+import sys
+
+from setuptools import setup
+
+
+setup_requires = []
+install_requires = [
+    'numpy>=1.9.0',
+]
+
+setup(
+    name='robot',
+    version='0.0.1',
+    description='A python robot programming library',
+    author='iory',
+    author_email='ab.ioryz@gmail.com',
+    url='https://github.com/iory/robot',
+    license='MIT License',
+    packages=['robot',
+              ],
+    zip_safe=False,
+    setup_requires=setup_requires,
+)
