1016433716e4ab1104b26fec3d8f4302a6ea1bf8
iory <ab.ioryz@gmail.com>
2017-11-03 13:52:23 +0900

    Add robot_model

--- robot/__init__.py
+++ robot/__init__.py
@@ -1,2 +1,8 @@
 from robot import coordinates
+
+worldcoords = coordinates.CascadedCoords()
+
 from robot import math
+from robot import robot_model
+from robot import utils
+from robot.utils.urdf import URDF
