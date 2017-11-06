28ef23c57c3d8ba0cf7283e6867fb0cd55550b48
iory <ab.ioryz@gmail.com>
2017-11-07 04:35:46 +0900

    [optimizers] Add optimizers

--- robot/__init__.py
+++ robot/__init__.py
@@ -3,6 +3,8 @@ from robot import coordinates
 worldcoords = coordinates.CascadedCoords()
 
 from robot import math
+from robot import optimizer
+from robot import optimizers
 from robot import robot_model
 from robot import utils
 from robot.utils.urdf import URDF
