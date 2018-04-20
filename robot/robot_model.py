49908c62d9c815f4172221351faacbe9caa8de4e
iory <ab.ioryz@gmail.com>
2018-04-21 00:02:54 +0900

    [robot_model] Add move_end_rot and move_end_pos for limb

--- robot/robot_model.py
+++ robot/robot_model.py
@@ -1260,6 +1260,23 @@ class RobotModel(CascadedLink):
         if len(links) > 0:
             worldcoords.add_child(self.link_list[0])
 
+    def move_end_pos(self, pos, wrt='local', *args, **kwargs):
+        pos = np.array(pos, dtype=np.float64)
+        self.inverse_kinematics(
+            self.end_coords.copy_worldcoords().translate(pos, wrt),
+            move_target=self.end_coords,
+            *args, **kwargs)
+        return self.angle_vector()
+
+    def move_end_rot(self, angle, axis, wrt='local', *args, **kwargs):
+        rotation_axis = kwargs.pop('rotation_axis', True)
+        self.inverse_kinematics(
+            self.end_coords.copy_worldcoords().rotate(angle, axis, wrt),
+            move_target=self.end_coords,
+            rotation_axis=rotation_axis,
+            *args, **kwargs)
+        return self.angle_vector()
+
     def fix_leg_to_coords(self, fix_coords, leg='both', mid=0.5):
         """
         Fix robot's legs to a coords
