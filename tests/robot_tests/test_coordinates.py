c3e8703c67d8c495ae3bf2e65d038a35b2bc6e01
iory <ab.ioryz@gmail.com>
2018-06-05 16:54:00 +0900

    [tests] Add transformation test

--- tests/robot_tests/test_coordinates.py
+++ tests/robot_tests/test_coordinates.py
@@ -1,6 +1,7 @@
 import unittest
 
 from numpy import testing
+from numpy import deg2rad
 
 from robot.coordinates import make_coords
 
@@ -12,3 +13,10 @@ class TestCoordinates(unittest.TestCase):
         coord.transform(make_coords(pos=[1, 2, 3]))
         testing.assert_array_equal(coord.pos,
                                    [1, 2, 3])
+
+    def test_transformation(self):
+        c = make_coords(rot=[deg2rad(10), 0, 0])
+        d = make_coords(rot=[deg2rad(20), 0, 0])
+        testing.assert_almost_equal(
+            c.transformation(d).worldrot(),
+            make_coords(rot=[deg2rad(10), 0, 0]).worldrot())
