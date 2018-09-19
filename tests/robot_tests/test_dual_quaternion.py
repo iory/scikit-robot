4cd6551a66812065e1619d0223d91c1d942d9487
iory <ab.ioryz@gmail.com>
2018-09-20 02:30:24 +0900

    [test/dual_quaternion] Add __add__ test

--- tests/robot_tests/test_dual_quaternion.py
+++ tests/robot_tests/test_dual_quaternion.py
@@ -12,6 +12,23 @@ from robot.math import quaternion_multiply
 
 class TestDualQuaternion(unittest.TestCase):
 
+    def test_add(self):
+        qr1 = normalize_vector(np.array([1, 2, 3, 4]))
+        x, y, z = np.array([1, 2, 3])
+        qd1 = 0.5 * quaternion_multiply(np.array([0, x, y, z]), qr1)
+        dq1 = DualQuaternion(qr1, qd1)
+
+        qr2 = normalize_vector(np.array([4, 3, 2, 1]))
+        x, y, z = np.array([3, 2, 1])
+        qd2 = 0.5 * quaternion_multiply(np.array([0, x, y, z]), qr2)
+        dq2 = DualQuaternion(qr2, qd2)
+
+        dq = (dq1 + dq2).normalize()
+        testing.assert_almost_equal(
+            dq.translation, [2.0, 2.6, 2.0])
+        testing.assert_almost_equal(
+            dq.quaternion, [0.5, 0.5, 0.5, 0.5])
+
     def test_mul(self):
         qr1 = normalize_vector(np.array([1, 2, 3, 4]))
         x, y, z = np.array([1, 2, 3])
