46baad180acb032a4242b6f1f82e2fbf189a38c4
iory <ab.ioryz@gmail.com>
2018-09-20 02:29:21 +0900

    [test/math] Add quaternion_norm

--- tests/robot_tests/test_math.py
+++ tests/robot_tests/test_math.py
@@ -17,6 +17,7 @@ from robot.math import quaternion_distance
 from robot.math import quaternion_from_axis_angle
 from robot.math import quaternion_inverse
 from robot.math import quaternion_multiply
+from robot.math import quaternion_norm
 from robot.math import quaternion_slerp
 from robot.math import rodrigues
 from robot.math import rotate_matrix
@@ -303,6 +304,13 @@ class TestMath(unittest.TestCase):
         q1 = rpy2quaternion([np.pi, 0, 0])
         q2 = rpy2quaternion([0, 0, 0])
         self.assertEqual(quaternion_distance(q1, q2), np.pi)
+
+    def test_quaternion_norm(self):
+        q = np.array([1, 0, 0, 0])
+        self.assertEqual(quaternion_norm(q), 1.0)
+
+        q = np.array([0, 0, 0, 0])
+        self.assertEqual(quaternion_norm(q), 0.0)
 
     def test_quaternion_from_axis_angle(self):
         q = quaternion_from_axis_angle(0.1, [1, 0, 0])
