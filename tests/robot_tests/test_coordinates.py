4d91e97f767f9d119ae4b91102639a89049e3808
iory <ab.ioryz@gmail.com>
2018-11-29 02:02:44 +0900

    [test] Add inverse_transformation

--- tests/robot_tests/test_coordinates.py
+++ tests/robot_tests/test_coordinates.py
@@ -22,6 +22,22 @@ class TestCoordinates(unittest.TestCase):
             c.transformation(d).worldrot(),
             make_coords(rot=[deg2rad(10), 0, 0]).worldrot())
 
+    def test_inverse_transformation(self):
+        coord_a = make_coords(pos=[1, 1, 1])
+        testing.assert_almost_equal(
+            coord_a.inverse_transformation().worldpos(),
+            [-1, -1, -1])
+
+        pos = [0.13264493, 0.05263172, 0.93042636]
+        q = [-0.20692513, 0.50841015, 0.82812527, 0.1136206]
+        coord_b = make_coords(pos=pos, rot=q)
+        testing.assert_almost_equal(
+            coord_b.inverse_transformation().worldpos(),
+            [-0.41549991, -0.12132025,  0.83588229])
+        testing.assert_almost_equal(
+            coord_b.inverse_transformation().quaternion,
+            [0.20692513, 0.50841015, 0.82812527, 0.1136206])
+
     def rotate(self):
         c = make_coords(pos=[1, 2, 3])
         c.rotate(pi / 7.0, 'y', 'world')
