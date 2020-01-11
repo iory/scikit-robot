38ddada2fbe7ba9fc944154e645be0b29c835232
Kentaro Wada <www.kentaro.wada@gmail.com>
2020-01-11 16:58:23 +0000

    Add PandaROSRobotInterface

--- skrobot/interfaces/__init__.py
+++ skrobot/interfaces/__init__.py
@@ -2,6 +2,12 @@
 
 from . import _pybullet as pybullet
 from ._pybullet import PybulletRobotInterface
+
+try:
+    from .ros import PandaROSRobotInterface
+except ImportError:
+    pass
+
 try:
     from .ros import PR2ROSRobotInterface
 except ImportError:
