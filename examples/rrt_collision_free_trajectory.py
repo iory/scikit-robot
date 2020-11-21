d16cd83cf2657acdb0a6e65174bf84bfd3f2dd7a
HiroIshida <spitfire.docomo@gmail.com>
2020-11-21 16:10:24 +0900

    bidirectional rrt (a.k.a RRT-connect) except collision checking

--- examples/rrt_collision_free_trajectory.py
+++ examples/rrt_collision_free_trajectory.py
@@ -9,9 +9,9 @@ if __name__ == '__main__':
     robot_model = skrobot.models.urdf.RobotModelFromURDF(
         urdf_file=skrobot.data.pr2_urdfpath())
     robot_model.init_pose()
-    #viewer = skrobot.viewers.TrimeshSceneViewer(resolution=(640, 480))
-    #viewer.add(robot_model)
-    #viewer.show()
+    viewer = skrobot.viewers.TrimeshSceneViewer(resolution=(640, 480))
+    viewer.add(robot_model)
+    viewer.show()
 
     link_idx_table = {}
     for link_idx in range(len(robot_model.link_list)):
@@ -36,25 +36,15 @@ if __name__ == '__main__':
         parent=robot_model.r_forearm_link)
 
     # set initial angle vector of rarm
-    av_init = [0.58, 0.35, -0.74, -0.70, -0.17, -0.63, 0.0]
+    av_init = np.array([-0.7] * 7)
+    av_goal = av_init + 0.5
     set_joint_angles(av_init)
-
-    target_coords = skrobot.coordinates.Coordinates(
-        [0.7, -0.7, 1.0], [0, 0, 0])
-    box_center = np.array([0.9, -0.2, 0.9])
-    box_width = np.array([0.5, 0.5, 0.6])
-    box = skrobot.models.Box(
-        extents=box_width, face_colors=(1., 0, 0)
-    )
-    box.translate(box_center)
-    #viewer.add(box)
-    margin = 0.1
-
-    def sdf(X):
-        return sdf_box(X, box_width * 0.5, box_center) - margin
-
-    joint_list = [link.joint for link in link_list]
-    joint_limits = [[max(j.min_angle, -3.14), min(j.max_angle, 3.14)] for j in joint_list]
-
     traj = robot_model.plan_trajectory_rrt(
-        target_coords, link_list, rarm_end_coords)
+        av_goal, link_list, rarm_end_coords, debug_plot=True)
+
+    time.sleep(1.0)
+    print("show trajectory")
+    for av in traj:
+        set_joint_angles(av)
+        viewer.redraw()
+        time.sleep(0.1)
--- skrobot/planner/sampling_based.py
+++ skrobot/planner/sampling_based.py
@@ -1,24 +1,28 @@
 import numpy as np
 from . import utils
+import matplotlib.pyplot as plt
 
 def plan_trajectory_rrt(self, 
-        target_coords,
+        av_goal,
         link_list,
-        end_effector
+        end_effector,
+        debug_plot=False
         ):
     joint_list = [link.joint for link in link_list]
     joint_mins = [max(j.min_angle, -3.14) for j in joint_list]
     joint_maxs = [min(j.max_angle, +3.14) for j in joint_list]
     cspace = ConfigurationSpace(joint_mins, joint_maxs)
     av_init = utils.get_robot_state(self, joint_list)
-    def pred_goal_condition(av):
-        position = utils.forward_kinematics(self, link_list, av, end_effector, False, False, False)
-        dist = np.linalg.norm(target_coords.worldpos() - position)
-        return dist < 0.1
-    pred_valid_config = lambda q : True
-    rrt = RapidlyExploringRandomTree(cspace, av_init, pred_goal_condition, pred_valid_config)
-    for i in range(1000):
-        print(rrt.extend())
+    pred_valid_config = lambda q : True # tmp, no obstacle
+
+    brrt = BidirectionalRRT(cspace, av_init, av_goal, pred_valid_config)
+    av_seq = brrt.solve()
+
+    if debug_plot:
+        fig, ax = brrt.show()
+        plt.show()
+
+    return av_seq
 
 class ConfigurationSpace(object):
     def __init__(self, b_min, b_max):
@@ -31,27 +35,42 @@ class ConfigurationSpace(object):
         return np.random.rand(self.n_dof) * w + self.b_min
 
 class RapidlyExploringRandomTree(object): 
-    def __init__(self, cspace, q_start, pred_goal_condition, pred_valid_config,
-            N_maxiter=10000):
+    def __init__(self, cspace, q_start, pred_goal_condition=None, pred_valid_config=None,
+            N_maxiter=30000):
         self.cspace = cspace
-        self.eps = 0.1
+        self.eps = 0.2
         self.n_resolution = 10
         self.N_maxiter = N_maxiter
+
+        if pred_goal_condition is None:
+            pred_goal_condition = lambda q: False
+
+        if pred_valid_config is None:
+            pred_valid_config = lambda q : True
+
         self.isValid = pred_valid_config
         self.isGoal = pred_goal_condition
 
         # reserving memory is the key 
-        self.Q_sample = np.zeros((N_maxiter, cspace.n_dof))
-        self.idxes_parents = np.zeros(N_maxiter, dtype='int64')
+        self._Q_sample = np.zeros((N_maxiter, cspace.n_dof))
+        self._idxes_parents = np.zeros(N_maxiter, dtype='int64')
 
         # set initial sample
         self.n_sample = 1
-        self.Q_sample[0] = q_start
-        self.idxes_parents[0] = 0 # self reference
+        self._Q_sample[0] = q_start
+        self._idxes_parents[0] = 0 # self reference
 
     @property
     def q_start(self):
-        return self.X_sample[0]
+        return self.Q_sample[0]
+
+    @property
+    def Q_sample(self): # safer 
+        return self._Q_sample[:self.n_sample]
+
+    @property
+    def idxes_parents(self): # safer 
+        return self._idxes_parents[:self.n_sample]
 
     def extend(self):
         if self.n_sample ==  self.N_maxiter:
@@ -61,9 +80,7 @@ class RapidlyExploringRandomTree(object):
             return vec/np.linalg.norm(vec)
 
         q_rand = self.cspace.sample()
-        q_rand_copied = np.repeat(q_rand.reshape(1, -1), self.n_sample, axis=0)
-
-        sqdists = np.sum((self.Q_sample[:self.n_sample] - q_rand_copied)**2, axis=1)
+        sqdists = np.sum((self.Q_sample - q_rand[None, :])**2, axis=1)
         idx_nearest = np.argmin(sqdists)
         q_nearest = self.Q_sample[idx_nearest]
         if np.linalg.norm(q_rand - q_nearest) > self.eps:
@@ -74,17 +91,84 @@ class RapidlyExploringRandomTree(object):
         # update tree
         q_new_reshaped = q_new.reshape(1, -1)
         if self.isValid(q_new_reshaped):
-            self.Q_sample[self.n_sample] = q_new
-            self.idxes_parents[self.n_sample] = idx_nearest
+            self._Q_sample[self.n_sample] = q_new
+            self._idxes_parents[self.n_sample] = idx_nearest
             self.n_sample += 1
+            return self.isGoal(q_new)
+        return False # not goal
 
-        return self.isGoal(q_new)
+    def backward_path(self, idx_end):
+        q_seq = [self.Q_sample[idx_end]]
+        idx_parent = self.idxes_parents[idx_end]
+        idx_root = 0
+        while idx_parent != idx_root:
+            q_seq.append(self.Q_sample[idx_parent])
+            idx_parent = self.idxes_parents[idx_parent]
+        q_seq.append(self.q_start)
+        return q_seq
 
-    def show(self):
-        fig, ax = plt.subplots()
+    def show(self, fax=None):
+        if fax is None:
+            fig, ax = plt.subplots()
+        else:
+            fig, ax = fax
         n = self.n_sample
-        ax.scatter(self.Q_sample[:n, 0], self.Q_sample [:n, 1], c="black")
-        for q, parent_idx in zip(self.Q_sample[:n], self.idxes_parents[:n]):
+        ax.scatter(self.Q_sample[:, 0], self.Q_sample[:, 1], c="black", s=5)
+        for q, parent_idx in zip(self.Q_sample, self.idxes_parents):
             q_parent = self.Q_sample[parent_idx]
-            ax.plot([q[0], q_parent[0]], [q[1], q_parent[1]], color="red")
+            ax.plot([q[0], q_parent[0]], [q[1], q_parent[1]], color="red", linewidth=0.5)
 
+class BidirectionalRRT(object):
+    def __init__(self, cspace, q_start, q_goal, pred_valid_config,
+            N_maxiter=10000):
+        self.rrt1 = RapidlyExploringRandomTree(cspace, q_start, pred_valid_config=pred_valid_config)
+        self.rrt2 = RapidlyExploringRandomTree(cspace, q_goal, pred_valid_config=pred_valid_config)
+
+        self.connect_pair = None # [idx_of_rrt1, idx_or_rrt2]
+        self.solution = None
+
+        # because setting it is mutual referencial, we can set 
+        # pred_goal_condition only after initialization of rrt1 and rrt2
+        def goalpred_for_rrt1(q):
+            diffs = self.rrt2.Q_sample - q[None, :]
+            sqdists = np.sum(diffs ** 2, axis=1)
+            idx_min = np.argmin(sqdists)
+            if sqdists[idx_min] < self.rrt1.eps ** 2:
+                idx_self = self.rrt1.n_sample - 1
+                idx_other = idx_min
+                self.connect_pair = [idx_self, idx_other]
+                return True
+            return False
+
+        self.rrt1.isGoal = goalpred_for_rrt1 # overwrite!
+
+    def solve(self):
+        while self.connect_pair is None:
+            self.rrt2.extend()
+            self.rrt1.extend()
+        print("solved")
+
+        sol_forward = self.rrt1.backward_path(self.connect_pair[0])
+        sol_forward.reverse()
+        sol_backward = self.rrt2.backward_path(self.connect_pair[1])
+        solution = sol_forward + sol_backward
+        self.solution = solution
+        return solution
+
+    def show(self, fax=None):
+        if fax is None:
+            fax = plt.subplots()
+        self.rrt1.show(fax)
+        self.rrt2.show(fax)
+        fig, ax = fax
+        if self.solution is not None:
+            for i in range(len(self.solution) - 1):
+                x = [self.solution[i][0], self.solution[i+1][0]]
+                y = [self.solution[i][1], self.solution[i+1][1]]
+                ax.plot(x, y, color="green")
+
+            # show connected pair
+            p1 = self.rrt1.Q_sample[self.connect_pair[0]]
+            p2 = self.rrt2.Q_sample[self.connect_pair[1]]
+            ax.plot([p1[0], p2[0]], [p1[1], p2[1]], color="blue")
+        return fig, ax
