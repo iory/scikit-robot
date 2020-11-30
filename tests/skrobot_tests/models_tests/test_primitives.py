de8c67f47279f04df0de2d03668cfb8fb34171b0
HiroIshida <spitfire.docomo@gmail.com>
2020-11-30 22:02:37 +0900

    update primitives test

--- tests/skrobot_tests/models_tests/test_primitives.py
+++ tests/skrobot_tests/models_tests/test_primitives.py
@@ -8,54 +8,54 @@ import trimesh
 class TestAxis(unittest.TestCase):
 
     def test_init(self):
-        skrobot.models.Axis()
+        skrobot.primitives.Axis()
 
     def from_coords(self):
         coords = skrobot.coordinates.Coordinates()
-        skrobot.models.Axis.from_coords(coords)
+        skrobot.primitives.Axis.from_coords(coords)
 
     def from_cascoords(self):
         cascoords = skrobot.coordinates.CascadedCoords()
-        skrobot.models.Axis.from_cascoords(cascoords)
+        skrobot.primitives.Axis.from_cascoords(cascoords)
 
 
 class TestBox(unittest.TestCase):
 
     def test_init(self):
-        skrobot.models.Box(extents=(1, 1, 1))
+        skrobot.primitives.Box(extents=(1, 1, 1))
 
 
 class TestCone(unittest.TestCase):
 
     def test_init(self):
-        skrobot.models.Cone(radius=0.5, height=1)
+        skrobot.primitives.Cone(radius=0.5, height=1)
 
 
 class TestCylinder(unittest.TestCase):
 
     def test_init(self):
-        skrobot.models.Cylinder(radius=0.5, height=1)
+        skrobot.primitives.Cylinder(radius=0.5, height=1)
 
 
 class TestSphere(unittest.TestCase):
 
     def test_init(self):
-        skrobot.models.Sphere(radius=1)
+        skrobot.primitives.Sphere(radius=1)
 
 
 class TestAnnulus(unittest.TestCase):
 
     def test_init(self):
-        skrobot.models.Annulus(r_min=0.2, r_max=0.5, height=1)
+        skrobot.primitives.Annulus(r_min=0.2, r_max=0.5, height=1)
 
 
 class TestMeshLink(unittest.TestCase):
 
     def test_init(self):
         cylinder = trimesh.creation.cylinder(radius=1.0, height=1.0)
-        skrobot.models.MeshLink(cylinder)
-        skrobot.models.MeshLink([cylinder, cylinder])
+        skrobot.primitives.MeshLink(cylinder)
+        skrobot.primitives.MeshLink([cylinder, cylinder])
 
         base_obj_path = osp.join(osp.dirname(skrobot.data.pr2_urdfpath()),
                                  'meshes', 'base_v0', 'base.obj')
-        skrobot.models.MeshLink(base_obj_path)
+        skrobot.primitives.MeshLink(base_obj_path)
