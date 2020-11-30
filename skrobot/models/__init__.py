5df2bf32852e424ff3fd9a201290616a8b1d054a
HiroIshida <spitfire.docomo@gmail.com>
2020-12-01 02:15:35 +0900

    a workaround for preserving the backward compatibility

--- skrobot/models/__init__.py
+++ skrobot/models/__init__.py
@@ -1,5 +1,10 @@
 # flake8: noqa
 
+# a workaround for preserving the backward-compatibility
+# so that a user also can import primitive by
+# import skrobot.models.primitive 
+from skrobot.model.primitives import *
+
 from skrobot.models.fetch import Fetch
 from skrobot.models.kuka import Kuka
 from skrobot.models.panda import Panda
