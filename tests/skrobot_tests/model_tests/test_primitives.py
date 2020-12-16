d74ec1301a4bfc2e80435b7f37490d89f18a2483
iory <ab.ioryz@gmail.com>
2020-12-16 18:37:47 +0900

    Rename skrobot/models/primitives.py to skrobot/model/primitives.py

--- tests/skrobot_tests/model_tests/test_primitives.py
+++ tests/skrobot_tests/model_tests/test_primitives.py
@@ -11,25 +11,25 @@ import trimesh
 class TestAxis(unittest.TestCase):
 
     def test_init(self):
-        skrobot.models.Axis()
+        skrobot.model.Axis()
 
     def from_coords(self):
         coords = skrobot.coordinates.Coordinates()
-        skrobot.models.Axis.from_coords(coords)
+        skrobot.model.Axis.from_coords(coords)
 
     def from_cascoords(self):
         cascoords = skrobot.coordinates.CascadedCoords()
-        skrobot.models.Axis.from_cascoords(cascoords)
+        skrobot.model.Axis.from_cascoords(cascoords)
 
 
 class TestBox(unittest.TestCase):
 
     def test_init(self):
-        skrobot.models.Box(extents=(1, 1, 1))
-        skrobot.models.Box(extents=(1, 1, 1), with_sdf=True)
+        skrobot.model.Box(extents=(1, 1, 1))
+        skrobot.model.Box(extents=(1, 1, 1), with_sdf=True)
 
     def test_init_with_sdf(self):
-        b = skrobot.models.Box(extents=(1, 1, 1), with_sdf=True)
+        b = skrobot.model.Box(extents=(1, 1, 1), with_sdf=True)
         booleans, _ = b.sdf.on_surface(b.visual_mesh.vertices)
         is_all_vertices_on_surface = np.all(booleans)
         self.assertTrue(is_all_vertices_on_surface)
@@ -38,22 +38,22 @@ class TestBox(unittest.TestCase):
 class TestCone(unittest.TestCase):
 
     def test_init(self):
-        skrobot.models.Cone(radius=0.5, height=1)
+        skrobot.model.Cone(radius=0.5, height=1)
 
 
 class TestCylinder(unittest.TestCase):
 
     def test_init(self):
-        skrobot.models.Cylinder(radius=0.5, height=1)
+        skrobot.model.Cylinder(radius=0.5, height=1)
 
 
 class TestSphere(unittest.TestCase):
 
     def test_init(self):
-        skrobot.models.Sphere(radius=1)
+        skrobot.model.Sphere(radius=1)
 
     def test_init_with_sdf(self):
-        s = skrobot.models.Sphere(radius=1.0, with_sdf=True)
+        s = skrobot.model.Sphere(radius=1.0, with_sdf=True)
         booleans, _ = s.sdf.on_surface(s.visual_mesh.vertices)
         is_all_vertices_on_surface = np.all(booleans)
         self.assertTrue(is_all_vertices_on_surface)
@@ -62,19 +62,19 @@ class TestSphere(unittest.TestCase):
 class TestAnnulus(unittest.TestCase):
 
     def test_init(self):
-        skrobot.models.Annulus(r_min=0.2, r_max=0.5, height=1)
+        skrobot.model.Annulus(r_min=0.2, r_max=0.5, height=1)
 
 
 class TestMeshLink(unittest.TestCase):
 
     def test_init(self):
         cylinder = trimesh.creation.cylinder(radius=1.0, height=1.0)
-        skrobot.models.MeshLink(cylinder)
-        skrobot.models.MeshLink([cylinder, cylinder])
+        skrobot.model.MeshLink(cylinder)
+        skrobot.model.MeshLink([cylinder, cylinder])
 
         base_obj_path = osp.join(osp.dirname(skrobot.data.pr2_urdfpath()),
                                  'meshes', 'base_v0', 'base.obj')
-        skrobot.models.MeshLink(base_obj_path)
+        skrobot.model.MeshLink(base_obj_path)
 
     def test_init_with_sdf(self):
         home_dir = osp.expanduser("~")
@@ -83,7 +83,7 @@ class TestMeshLink(unittest.TestCase):
             shutil.rmtree(sdf_cache_dir)
 
         bunny_obj_path = skrobot.data.bunny_objpath()
-        m = skrobot.models.MeshLink(bunny_obj_path, with_sdf=True, dim_grid=50)
+        m = skrobot.model.MeshLink(bunny_obj_path, with_sdf=True, dim_grid=50)
 
         booleans, _ = m.sdf.on_surface(m.visual_mesh.vertices)
         is_all_vertices_on_surface = np.all(booleans)
