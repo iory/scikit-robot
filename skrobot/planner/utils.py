b2ea81628be7958e10a25a3d97691d2b7138e169
HiroIshida <spitfire.docomo@gmail.com>
2020-12-18 01:34:16 +0900

    Add utils for planning

---
+++ skrobot/planner/utils.py
@@ -0,0 +1,193 @@
+import numpy as np
+
+from skrobot.coordinates import CascadedCoords
+from skrobot.coordinates import Coordinates
+from skrobot.coordinates.math import rpy_angle
+from skrobot.coordinates.math import rpy_matrix
+
+
+def set_robot_config(robot_model, joint_list, av, with_base=False):
+    """A utility function for setting robot state
+
+    Parameters
+    ----------
+    robot_model : skrobot.model.CascadedLink
+        robot model
+    joint_list : list[skrobot.model.Joint]
+        joint list to be set
+    av : numpy.ndarray[float](n_dof,)
+        angle vector which has n_dof dims.
+    with_base : bool
+        If `with_base=False`, `n_dof` is the number of joints `n_joint`,
+        but if `with_base=True`, `n_dof = n_joint + 3`.
+    """
+
+    if with_base:
+        assert len(joint_list) + 3 == len(av)
+    else:
+        assert len(joint_list) == len(av)
+
+    if with_base:
+        av_joint, av_base = av[:-3], av[-3:]
+        x, y, theta = av_base
+        co = Coordinates(pos=[x, y, 0.0], rot=rpy_matrix(theta, 0.0, 0.0))
+        robot_model.newcoords(co)
+    else:
+        av_joint = av
+
+    for joint, angle in zip(joint_list, av_joint):
+        joint.joint_angle(angle)
+
+
+def get_robot_config(robot_model, joint_list, with_base=False):
+    """A utility function for getting robot state
+
+    Parameters
+    ----------
+    robot_model : skrobot.model.CascadedLink
+        robot model
+    joint_list : list[Joint]
+        joint list of which you want to know the angles
+    with_base : bool
+        If set to `True`, base position is also computed.
+
+    Returns
+    -------
+    av_whole (or av_joint) : numpy.array(n_dof,)
+        angle vector. If `with_base=False`, `n_dof` is the number of
+        joints `n_joint`, but if `with_base=True`, `n_dof = n_joint + 3`.
+    """
+    av_joint = np.array([j.joint_angle() for j in joint_list])
+    if with_base:
+        x, y, _ = robot_model.translation
+        rpy = rpy_angle(robot_model.rotation)[0]
+        theta = rpy[0]
+        av_whole = np.hstack((av_joint, [x, y, theta]))
+        return av_whole
+    else:
+        return av_joint
+
+
+def forward_kinematics_multi(robot_model,
+                             joint_list,
+                             av,
+                             move_target_list,
+                             with_rot,
+                             with_base,
+                             with_jacobian):
+    """Compute fk for multiple feature points
+
+    Parameters
+    ----------
+    robot_model : skrobot.model.CascadedLink
+        robot model.
+    joint_list : list[skrobot.model.Joint]
+        joint to be controlled
+    av : numpy.ndarray(n_dof,)
+        angle vector.
+    move_target_list : list[skrobot.coordinates.CascadedCoords]
+        the list has `n_feature` elements. Each element is the
+        coordinate of the features points.
+    with_rot : bool
+        If set to `True`, 7(3 + 4) dim pose-fk is also computed.
+        Otherwise, 3 dim point-fk is computed.
+    with_base : bool
+        If `with_base=False`, `n_dof` is the number of joints `n_joint`,
+        but if `with_base=True`, `n_dof = n_joint + 3`.
+
+    Returns
+    -------
+    pose_arr : numpy.ndarray(n_feature, dim_pose)
+        array of pose of each feature points.
+        `dim_pose=7' if `with_rot=True`. Otherwise, `dim_pose=3`.
+    jac_arr : numpy.ndarray(n_feature, dim_pose, n_dof)
+        array of jacobian of each feature points.
+
+    """
+
+    set_robot_config(robot_model, joint_list, av, with_base)
+    n_feature = len(move_target_list)
+    n_dof = len(av)
+    dim_pose = 3 + (4 if with_rot else 0)
+
+    pose_arr = np.zeros((n_feature, dim_pose))
+    if with_jacobian:
+        jac_arr = np.zeros((n_feature, dim_pose, n_dof))
+    else:
+        jac_arr = None
+
+    for i in range(n_feature):
+        pose, jac = _forward_kinematics(
+            robot_model, joint_list, move_target_list[i],
+            with_rot, with_base, with_jacobian)
+        pose_arr[i, :] = pose
+        if with_jacobian:
+            jac_arr[i, :, :] = jac
+    return pose_arr, jac_arr
+
+
+def _forward_kinematics(robot_model,
+                        joint_list,
+                        move_target,
+                        with_rot,
+                        with_base,
+                        with_jacobian):
+
+    link_list = [joint.child_link for joint in joint_list]
+
+    ef_pos_wrt_world = move_target.worldpos()
+    ef_quat_wrt_world = move_target.worldcoords().quaternion
+    world_coordinate = CascadedCoords()
+
+    def quaternion_kinematic_matrix(q):
+        # dq/dt = 0.5 * mat * omega
+        q1, q2, q3, q4 = q
+        mat = np.array([
+            [-q2, -q3, -q4],
+            [q1, q4, -q3],
+            [-q4, q1, q2],
+            [q3, -q2, q1]])
+        return mat * 0.5
+
+    def compute_jacobian_wrt_world():
+        J_joint = robot_model.calc_jacobian_from_link_list(
+            [move_target],
+            link_list,
+            transform_coords=world_coordinate,
+            rotation_axis=with_rot)
+
+        if with_rot:
+            kine_mat = quaternion_kinematic_matrix(ef_quat_wrt_world)
+            J_joint_rot_geometric = J_joint[3:, :]  # "geometric" jacobian
+            J_joint_quat = kine_mat.dot(J_joint_rot_geometric)
+            J_joint = np.vstack((J_joint[:3, :], J_joint_quat))
+
+        if with_base:  # cat base jacobian if base is considered
+            # please follow computation carefully
+            base_pos_wrt_world = robot_model.worldpos()
+            ef_pos_wrt_world = move_target.worldpos()
+            ef_pos_wrt_base = ef_pos_wrt_world - base_pos_wrt_world
+            x, y = ef_pos_wrt_base[0], ef_pos_wrt_base[1]
+            J_base_pos = np.array([[1, 0, -y], [0, 1, x], [0, 0, 0]])
+
+            if with_rot:
+                J_base_quat_xy = np.zeros((4, 2))
+                rot_axis = np.array([0, 0, 1.0])
+                J_base_quat_theta = kine_mat.dot(rot_axis).reshape(4, 1)
+                J_base_quat = np.hstack((J_base_quat_xy, J_base_quat_theta))
+                J_base = np.vstack((J_base_pos, J_base_quat))
+            else:
+                J_base = J_base_pos
+            J_whole = np.hstack((J_joint, J_base))
+        else:
+            J_whole = J_joint
+        return J_whole
+
+    pose = np.hstack((ef_pos_wrt_world, ef_quat_wrt_world)) if with_rot \
+        else ef_pos_wrt_world
+
+    if with_jacobian:
+        J = compute_jacobian_wrt_world()
+        return pose, J
+    else:
+        return pose, None
---
+++ tests/skrobot_tests/planner_tests/test_utils.py
@@ -0,0 +1,125 @@
+import copy
+import unittest
+
+import numpy as np
+from numpy import testing
+
+import skrobot
+from skrobot.planner.utils import forward_kinematics_multi
+from skrobot.planner.utils import get_robot_config
+from skrobot.planner.utils import set_robot_config
+
+
+def jacobian_test_util(func, x0, decimal=5):
+    # test jacobian by comparing the resulting and numerical jacobian
+    f0, jac = func(x0)
+    n_dim = len(x0)
+
+    eps = 1e-7
+    jac_numerical = np.zeros(jac.shape)
+    for idx in range(n_dim):
+        x1 = copy.copy(x0)
+        x1[idx] += eps
+        f1, _ = func(x1)
+        jac_numerical[:, idx] = (f1 - f0) / eps
+    testing.assert_almost_equal(jac, jac_numerical, decimal=decimal)
+
+
+class TestPlannerUtils(unittest.TestCase):
+
+    @classmethod
+    def setup_class(cls):
+        robot_model = skrobot.models.PR2()
+
+        link_idx_table = {}
+        for link_idx in range(len(robot_model.link_list)):
+            name = robot_model.link_list[link_idx].name
+            link_idx_table[name] = link_idx
+
+        link_names = ["r_shoulder_pan_link", "r_shoulder_lift_link",
+                      "r_upper_arm_roll_link", "r_elbow_flex_link",
+                      "r_forearm_roll_link", "r_wrist_flex_link",
+                      "r_wrist_roll_link"]
+
+        link_list = [robot_model.link_list[link_idx_table[lname]]
+                     for lname in link_names]
+        joint_list = [l.joint for l in link_list]
+
+        cls.robot_model = robot_model
+        cls.link_list = link_list
+        cls.joint_list = joint_list
+
+        av = np.array([0.4, 0.6] + [-0.7] * 5)
+        cls.av = np.array([0.4, 0.6] + [-0.7] * 5)
+        cls.av_with_base = np.hstack((av, [0.1, 0.0, 0.3]))
+
+    def test_set_and_get_robot_config(self):
+        robot_model = self.robot_model
+        joint_list = self.joint_list
+        av = self.av
+        av_with_base = self.av_with_base
+
+        with self.assertRaises(AssertionError):
+            set_robot_config(robot_model, joint_list, av, with_base=True)
+        set_robot_config(robot_model, joint_list, av, with_base=False)
+
+        with self.assertRaises(AssertionError):
+            set_robot_config(robot_model, joint_list,
+                             av_with_base, with_base=False)
+        set_robot_config(robot_model, joint_list, av_with_base, with_base=True)
+
+        testing.assert_equal(
+            av,
+            get_robot_config(robot_model, joint_list, with_base=False)
+        )
+        testing.assert_equal(
+            av_with_base,
+            get_robot_config(robot_model, joint_list, with_base=True)
+        )
+
+    def test_forward_kinematics_multi(self):
+        robot_model = self.robot_model
+        link_list = self.link_list
+        joint_list = self.joint_list
+        av_init = self.av
+        av_with_base_init = self.av_with_base
+
+        move_target_list = link_list
+        n_feature = len(move_target_list)
+
+        def fk_fun_simple(av, with_base, with_rot, with_jacobian):
+            pose_arr, jac_arr = forward_kinematics_multi(
+                robot_model, joint_list, av, move_target_list,
+                with_rot, with_base, with_jacobian)
+            return pose_arr, jac_arr
+
+        # checking returning types and shapes:
+        for with_base, av in [(False, av_init), (True, av_with_base_init)]:
+            for with_rot, n_pose in [(False, 3), (True, 3 + 4)]:
+                for with_jacobian in [False, True]:
+                    p, jac = fk_fun_simple(
+                        av, with_base, with_rot, with_jacobian)
+                    self.assertEqual(p.shape, (n_feature, n_pose))
+                    n_dof = len(av)
+                    if with_jacobian:
+                        self.assertEqual(jac.shape, (n_feature, n_pose, n_dof))
+                    else:
+                        self.assertEqual(jac, None)
+
+        def fk_jac_test(av, with_base, with_rot):
+            n_dof = len(av)
+            pose_arr, jac_arr = fk_fun_simple(av, with_base, with_rot, True)
+            pose_flatten = pose_arr.flatten()  # (pose_dim * n_feature)
+            pose_dim = 7 if with_rot else 3
+            jac_flatten = jac_arr.reshape(n_feature * pose_dim, n_dof)
+            return pose_flatten, jac_flatten
+
+        # checking jacobian
+        jacobian_test_util(
+            lambda av: fk_jac_test(av, False, False), av_init)
+        jacobian_test_util(
+            lambda av: fk_jac_test(av, False, True), av_init)
+        jacobian_test_util(
+            lambda av: fk_jac_test(av, True, False), av_with_base_init)
+        jacobian_test_util(
+            lambda av: fk_jac_test(av, True, True), av_with_base_init)
