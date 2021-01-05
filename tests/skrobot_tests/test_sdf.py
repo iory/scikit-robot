811df23f63178dc9dc98adc0ba0edc9d733a4ac1
HiroIshida <spitfire.docomo@gmail.com>
2021-01-05 15:41:34 +0900

    Created skrobot_tests/data

--- tests/skrobot_tests/test_sdf.py
+++ tests/skrobot_tests/test_sdf.py
@@ -215,7 +215,8 @@ class TestSDF(unittest.TestCase):
     def test_union_sdf_from_robot_model(self):
         here_full_filepath = os.path.join(os.getcwd(), __file__)
         here_full_dirpath = os.path.dirname(here_full_filepath)
-        test_model_path = os.path.join(here_full_dirpath, 'primitives.urdf')
+        test_model_path = os.path.join(
+            here_full_dirpath, '..', 'data', 'primitives.urdf')
 
         primitive_robot = skrobot.models.urdf.RobotModelFromURDF(
             urdf_file=test_model_path)
