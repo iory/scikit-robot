9ae3a52d0cfb04210ec421077c28d41d6ea3df81
HiroIshida <spitfire.docomo@gmail.com>
2021-01-09 00:57:58 +0900

    Update demo

--- examples/collision_free_trajectory.py
+++ examples/collision_free_trajectory.py
@@ -29,6 +29,8 @@ np.random.seed(0)
 robot_model = skrobot.models.PR2()
 robot_model.init_pose()
 
+# create obstacle's visual element with the corresponding sdf.
+# The sdf is stored as a member variable of box.
 box_center = np.array([0.9, -0.2, 0.9])
 box = Box(extents=[0.7, 0.5, 0.6], with_sdf=True)
 box.translate(box_center)
@@ -49,30 +51,34 @@ coll_link_list = [
 with_base = not args.without_base
 av_start = np.array([0.564, 0.35, -0.74, -0.7, -0.7, -0.17, -0.63])
 if with_base:
-    av_start = np.hstack([av_start, [0, 0, 0]])
+    # base pose is sepcified by [x, y, theta]
+    base_pose_start = [-0.5, 0.8, 0]
+    av_start = np.hstack([av_start, base_pose_start])
 
 # solve inverse kinematics to obtain av_goal
 joint_angles = np.deg2rad([-60, 74, -70, -120, -20, -30, 180])
 set_robot_config(robot_model, joint_list, joint_angles)
+target_coords = skrobot.coordinates.Coordinates([0.8, -0.6, 0.8], [0, 0, 0])
 
 rarm_end_coords = skrobot.coordinates.CascadedCoords(
     parent=robot_model.r_gripper_tool_frame,
     name='rarm_end_coords')
 robot_model.inverse_kinematics(
-    target_coords=skrobot.coordinates.Coordinates([0.8, -0.6, 0.8], [0, 0, 0]),
+    target_coords=target_coords,
     link_list=link_list,
     move_target=robot_model.rarm_end_coords, rotation_axis=True)
 av_goal = get_robot_config(robot_model, joint_list, with_base=with_base)
 
 # collision checker setup
-sscc = SweptSphereSdfCollisionChecker(lambda X: box.sdf(X), robot_model)
+sscc = SweptSphereSdfCollisionChecker(box.sdf, robot_model)
 for link in coll_link_list:
     sscc.add_collision_link(link)
 
 # motion planning
 ts = time.time()
+n_waypoint = 10
 av_seq = sqp_plan_trajectory(
-    sscc, av_start, av_goal, joint_list, 10,
+    sscc, av_start, av_goal, joint_list, n_waypoint,
     safety_margin=1e-2, with_base=with_base)
 print("solving time : {0} sec".format(time.time() - ts))
 
@@ -81,7 +87,7 @@ print("show trajectory")
 viewer = skrobot.viewers.TrimeshSceneViewer(resolution=(640, 480))
 viewer.add(robot_model)
 viewer.add(box)
-viewer.add(Axis(pos=[0.8, -0.6, 0.8]))
+viewer.add(Axis(pos=target_coords.worldpos(), rot=target_coords.worldrot()))
 sscc.add_coll_spheres_to_viewer(viewer)
 viewer.show()
 for av in av_seq:
