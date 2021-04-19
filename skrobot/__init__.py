29c4445337a58dd6cd9cce5ca32f829801667f3e
HiroIshida <spitfire.docomo@gmail.com>
2021-04-19 11:01:32 +0900

    Import planner

--- skrobot/__init__.py
+++ skrobot/__init__.py
@@ -11,6 +11,7 @@ from skrobot import data
 from skrobot import interpolator
 from skrobot import optimizer
 from skrobot import optimizers
+from skrobot import planner
 from skrobot import interfaces
 from skrobot import model
 from skrobot import models
