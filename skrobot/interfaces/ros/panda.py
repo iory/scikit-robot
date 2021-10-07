1b0067b6d94f1b537f82238bcefa3d8854046907
Kentaro Wada <www.kentaro.wada@gmail.com>
2021-10-07 10:31:19 +0100

    Add grasp/ungrasp to PandaROSRobotInterface

--- skrobot/interfaces/ros/panda.py
+++ skrobot/interfaces/ros/panda.py
@@ -1,9 +1,27 @@
+import actionlib
 import control_msgs.msg
+import franka_gripper.msg
 
 from .base import ROSRobotInterfaceBase
 
 
+WIDTH_MAX = 0.08
+
+
 class PandaROSRobotInterface(ROSRobotInterfaceBase):
+
+    def __init__(self, *args, **kwargs):
+        super(PandaROSRobotInterface, self).__init__(*args, **kwargs)
+
+        self.gripper_move = actionlib.SimpleActionClient(
+            'franka_gripper/move',
+            franka_gripper.msg.MoveAction)
+        self.gripper_move.wait_for_server()
+
+        self.gripper_stop = actionlib.SimpleActionClient(
+            'franka_gripper/stop',
+            franka_gripper.msg.StopAction)
+        self.gripper_stop.wait_for_server()
 
     @property
     def rarm_controller(self):
@@ -17,3 +35,23 @@ class PandaROSRobotInterface(ROSRobotInterfaceBase):
 
     def default_controller(self):
         return [self.rarm_controller]
+
+    def grasp(self, width=0, **kwargs):
+        self.move_gripper(width=width, **kwargs)
+
+    def ungrasp(self, **kwargs):
+        self.move_gripper(width=WIDTH_MAX, **kwargs)
+
+    def move_gripper(self, width, speed=WIDTH_MAX, wait=True):
+        goal = franka_gripper.msg.MoveGoal(width=width, speed=speed)
+        if wait:
+            self.gripper_move.send_goal_and_wait(goal)
+        else:
+            self.gripper_move.send_goal(goal)
+
+    def stop_gripper(self, wait=True):
+        goal = franka_gripper.msg.StopGoal()
+        if wait:
+            self.gripper_move.send_goal_and_wait(goal)
+        else:
+            self.gripper_move.send_goal(goal)
