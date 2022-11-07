d58e50a2e914c2a93c5ce6b408e55bb873da691d
HiroIshida <spitfire.docomo@gmail.com>
2022-11-08 01:03:04 +0900

    Dummy function will be set if cvxopt module not found

--- skrobot/optimizers/__init__.py
+++ skrobot/optimizers/__init__.py
@@ -1,2 +1,10 @@
-from skrobot.optimizers import cvxopt_solver  # NOQA
-from skrobot.optimizers import quadprog_solver  # NOQA
+def dummy_func(*args, **kwargs):
+    message = "cvxopt is not installed. please install cvxopt by 'pip install cvxopt' if wheel is avilable for your platform"
+    raise ModuleNotFoundError(message)
+
+try:
+    from skrobot.optimizers import cvxopt_solver  # NOQA
+    from skrobot.optimizers import quadprog_solver  # NOQA
+except ImportError:
+    cvxopt_solver = dummy_func
+    quadprog_solver = dummy_func
