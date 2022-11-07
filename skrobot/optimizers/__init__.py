3bdd28c52e6abce6866a2fab682d477d8c41785f
HiroIshida <spitfire.docomo@gmail.com>
2022-11-08 01:58:01 +0900

    Use ImportError instead

--- skrobot/optimizers/__init__.py
+++ skrobot/optimizers/__init__.py
@@ -2,12 +2,12 @@ def dummy_func(*args, **kwargs):
     message = "cvxopt is not installed. "\
               "Please install cvxopt by 'pip install cvxopt' "\
               "if wheel is released for your platform."
-    raise ModuleNotFoundError(message)
+    raise ImportError(message)
 
 
 try:
     from skrobot.optimizers import cvxopt_solver  # NOQA
     from skrobot.optimizers import quadprog_solver  # NOQA
-except (ImportError, ModuleNotFoundError):
+except ImportError:
     cvxopt_solver = dummy_func
     quadprog_solver = dummy_func
