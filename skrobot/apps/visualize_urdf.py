033e7e94ae2d7202b6a95bb08b4a790bb4e36953
Iori Yanokura <ab.ioryz@gmail.com>
2024-01-07 18:36:15 +0900

    Add viewer option for visualize-urdf

--- skrobot/apps/visualize_urdf.py
+++ skrobot/apps/visualize_urdf.py
@@ -10,9 +10,16 @@ from skrobot.models.urdf import RobotModelFromURDF
 def main():
     parser = argparse.ArgumentParser(description='Visualize URDF')
     parser.add_argument('input_urdfpath', type=str, help='Input URDF path')
+    parser.add_argument(
+        '--viewer', type=str,
+        choices=['trimesh', 'pyrender'], default='trimesh',
+        help='Choose the viewer type: trimesh or pyrender')
     args = parser.parse_args()
 
-    viewer = skrobot.viewers.TrimeshSceneViewer()
+    if args.viewer == 'trimesh':
+        viewer = skrobot.viewers.TrimeshSceneViewer()
+    elif args.viewer == 'pyrender':
+        viewer = skrobot.viewers.PyrenderViewer()
     model = RobotModelFromURDF(urdf_file=osp.abspath(args.input_urdfpath))
     viewer.add(model)
     viewer._init_and_start_app()
