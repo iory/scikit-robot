973dcf2ea85cbe01d19d330977b06f64621089cc
Iori Yanokura <ab.ioryz@gmail.com>
2025-09-22 22:52:36 +0900

    Lazy import pybulelt for interfaces

--- skrobot/interfaces/__init__.py
+++ skrobot/interfaces/__init__.py
@@ -1,6 +1,36 @@
 # flake8: noqa
 
-from . import _pybullet as pybullet
-from ._pybullet import PybulletRobotInterface
+import importlib
+
+
+class LazyPybulletModule(object):
+    def __init__(self):
+        self._module = None
+
+    def __getattr__(self, attr):
+        if self._module is None:
+            self._module = importlib.import_module('skrobot.interfaces._pybullet')
+        return getattr(self._module, attr)
+
+    def __dir__(self):
+        if self._module is None:
+            return []
+        return dir(self._module)
+
+
+# Lazy import for pybullet module
+pybullet = LazyPybulletModule()
+
+
+def __getattr__(name):
+    if name == 'PybulletRobotInterface':
+        return pybullet.PybulletRobotInterface
+    elif name == '_pybullet':
+        # Force lazy loading - only import when explicitly requested
+        if pybullet._module is None:
+            pybullet._module = importlib.import_module('skrobot.interfaces._pybullet')
+        return pybullet._module
+    raise AttributeError(f"module 'skrobot.interfaces' has no attribute '{name}'")
+
 
 from .ros import *
