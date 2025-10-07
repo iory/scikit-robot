37890231c5f4e349f927c51878b2027b6b27b441
Iori Yanokura <ab.ioryz@gmail.com>
2025-10-07 13:37:50 +0900

    Add 'skr transform-urdf command' to change urdf rotation with respect to world

--- skrobot/urdf/__init__.py
+++ skrobot/urdf/__init__.py
@@ -1,6 +1,7 @@
 from .aggregate import aggregate_urdf_mesh_files
 from .modularize_urdf import find_root_link
 from .modularize_urdf import transform_urdf_to_macro
+from .transform_urdf import transform_urdf_with_world_link
 from .wheel_collision_converter import convert_wheel_collisions_to_cylinders
 from .wheel_collision_converter import get_mesh_dimensions
 from .xml_root_link_changer import change_urdf_root_link
@@ -12,6 +13,7 @@ __all__ = [
     'URDFXMLRootLinkChanger',
     'find_root_link',
     'transform_urdf_to_macro',
+    'transform_urdf_with_world_link',
     'aggregate_urdf_mesh_files',
     'convert_wheel_collisions_to_cylinders',
     'get_mesh_dimensions',
