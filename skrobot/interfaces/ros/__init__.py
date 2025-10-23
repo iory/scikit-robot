20edafd43bd9219f0b29e78fdc277b8fdd015466
Michi-Tsubaki <michi.tsubaki.tech@gmail.com>
2025-10-23 23:59:15 +0900

    Add nextage robot model and interface

--- skrobot/interfaces/ros/__init__.py
+++ skrobot/interfaces/ros/__init__.py
@@ -1,9 +1,10 @@
 from skrobot._lazy_imports import LazyImportClass
 
-
+NextageROSRobotInterface = LazyImportClass(
+    ".nextage", "NextageROSRobotInterface", "skrobot.interfaces.ros")
 PandaROSRobotInterface = LazyImportClass(
     ".panda", "PandaROSRobotInterface", "skrobot.interfaces.ros")
 PR2ROSRobotInterface = LazyImportClass(
     ".pr2", "PR2ROSRobotInterface", "skrobot.interfaces.ros")
 
-__all__ = ["PandaROSRobotInterface", "PR2ROSRobotInterface"]
+__all__ = ["NextageROSRobotInterface", "PandaROSRobotInterface", "PR2ROSRobotInterface"]
