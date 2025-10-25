e6c46730b4ac53cf296db11d5ba3ac238f540741
Michi-Tsubaki <michi.tsubaki.tech@gmail.com>
2025-10-25 10:47:17 +0900

    Fix endcoords and reset_pose

--- skrobot/models/nextage.py
+++ skrobot/models/nextage.py
@@ -1,12 +1,13 @@
 from cached_property import cached_property
+import numpy as np
 
+from ..coordinates import Coordinates
 from ..data import nextage_urdfpath
 from ..model import RobotModel
 from .urdf import RobotModelFromURDF
 
 
 class Nextage(RobotModelFromURDF):
-
     """
     - Nextage Open Official Information.
 
@@ -26,12 +27,22 @@ class Nextage(RobotModelFromURDF):
         return nextage_urdfpath()
 
     def reset_pose(self):
-        import numpy as np
         angle_vector = [
             0.0,
-            -0.5, 0.0, np.deg2rad(-100), np.deg2rad(15.2), np.deg2rad(9.4), np.deg2rad(3.2),
-            0.5, 0.0, np.deg2rad(-100), np.deg2rad(-15.2), np.deg2rad(9.4), np.deg2rad(-3.2),
-            0.0, 0.0
+            0.0,
+            0.0,
+            np.deg2rad(0.6),
+            0.0,
+            np.deg2rad(-100),
+            np.deg2rad(-15.2),
+            np.deg2rad(9.4),
+            np.deg2rad(-3.2),
+            np.deg2rad(-0.6),
+            0.0,
+            np.deg2rad(-100),
+            np.deg2rad(15.2),
+            np.deg2rad(9.4),
+            np.deg2rad(3.2),
         ]
         self.angle_vector(angle_vector)
         return self.angle_vector()
@@ -42,7 +53,7 @@ class Nextage(RobotModelFromURDF):
 
     @cached_property
     def rarm(self):
-        link_names = ['RARM_JOINT{}_Link'.format(i) for i in range(6)]
+        link_names = ["RARM_JOINT{}_Link".format(i) for i in range(6)]
         links = [getattr(self, n) for n in link_names]
         joints = [l.joint for l in links]
         model = RobotModel(link_list=links, joint_list=joints)
@@ -51,7 +62,7 @@ class Nextage(RobotModelFromURDF):
 
     @cached_property
     def larm(self):
-        link_names = ['LARM_JOINT{}_Link'.format(i) for i in range(6)]
+        link_names = ["LARM_JOINT{}_Link".format(i) for i in range(6)]
         links = [getattr(self, n) for n in link_names]
         joints = [l.joint for l in links]
         model = RobotModel(link_list=links, joint_list=joints)
@@ -60,7 +71,7 @@ class Nextage(RobotModelFromURDF):
 
     @cached_property
     def head(self):
-        link_names = ['HEAD_JOINT{}_Link'.format(i) for i in range(2)]
+        link_names = ["HEAD_JOINT{}_Link".format(i) for i in range(2)]
         links = [getattr(self, n) for n in link_names]
         joints = [l.joint for l in links]
         model = RobotModel(link_list=links, joint_list=joints)
@@ -69,11 +80,21 @@ class Nextage(RobotModelFromURDF):
 
     @cached_property
     def rarm_end_coords(self):
-        return self.RARM_JOINT5_Link
+        end_coords = Coordinates()
+        self.RARM_JOINT5_Link.assoc(end_coords)
+        end_coords.newcoords(self.RARM_JOINT5_Link.copy_worldcoords())
+        end_coords.translate([-0.185, 0.0, -0.01])
+        end_coords.rotate(-np.pi / 2.0, 'y')
+        return end_coords
 
     @cached_property
     def larm_end_coords(self):
-        return self.LARM_JOINT5_Link
+        end_coords = Coordinates()
+        self.LARM_JOINT5_Link.assoc(end_coords)
+        end_coords.newcoords(self.LARM_JOINT5_Link.copy_worldcoords())
+        end_coords.translate([-0.185, 0.0, -0.01])
+        end_coords.rotate(-np.pi / 2.0, 'y')
+        return end_coords
 
     @cached_property
     def head_end_coords(self):
