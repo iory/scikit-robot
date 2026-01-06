dff80b3ee3c41cd521a418bdd05ac9101085f203
Iori Yanokura <ab.ioryz@gmail.com>
2026-01-06 18:20:45 +0900

    Delete generate-robot-class command

--- setup.py
+++ setup.py
@@ -89,7 +89,6 @@ console_scripts.append("change-urdf-root=skrobot.apps.change_urdf_root:main")
 console_scripts.append("visualize-mesh=skrobot.apps.visualize_mesh:main")
 console_scripts.append("urdf-hash=skrobot.apps.urdf_hash:main")
 console_scripts.append("convert-wheel-collision=skrobot.apps.convert_wheel_collision:main")
-console_scripts.append("generate-robot-class=skrobot.apps.generate_robot_class:main")
 
 
 setup(
